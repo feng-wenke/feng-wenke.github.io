0dd369a02cd9b340a364f9b30ccc5a1ea53e51dc
Frank Wen <126837295+Frank-wenke@users.noreply.github.com>
2024-06-05 19:13:06 +0800

    Update randomplay.js

--- js/randomplay.js
+++ js/randomplay.js
@@ -1,11 +1,21 @@
 
+// 随机播放音乐的函数
 function playRandomMusic() {
-  // 生成一个随机索引
   var audioPlayer = document.getElementById('audioPlayer');
-  var musicList = audioPlayer.querySelectorAll('source');
+  var musicList = [
+  '../video/晚夜微风问海棠.mp3',
+  '../video/半壶纱.mp3',
+  '../video/精卫.mp3',
+ ' ../video/记念.mp3',
+  '../video/阿嬷.mp3'
+  // 更多音乐文件...
+];
   var randomIndex = Math.floor(Math.random() * musicList.length);
+  var musicPath = musicList[randomIndex];
   // 更新音频源的 src 属性为随机选择的音乐文件
-  audioPlayer.src = musicList[randomIndex].src;
-  audioPlayer.load();
-  audioPlayer.play();
+ audioPlayer.src = musicPath;
+  // 播放音乐
+  audioPlayer.play().catch(error => {
+    console.error('播放音乐时出错:', error);
+  });
 }
