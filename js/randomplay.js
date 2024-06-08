4edbf262e366647336b25c7fea40121609127be3
Frank Wen <126837295+Frank-wenke@users.noreply.github.com>
2024-06-08 14:22:38 +0800

    Update randomplay.js

--- js/randomplay.js
+++ js/randomplay.js
@@ -59,3 +59,17 @@ function togglePlayPause() {
 }
 
 // 播放/暂停
+
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('myButton');
+
+    button.addEventListener('click', function() {
+        // 添加.temp-background类
+        this.classList.add('temp-background');
+
+        // 设置一个计时器，用于在0.1秒后移除.temp-background类
+        setTimeout(function() {
+            button.classList.remove('temp-background');
+        }, 100); // 0.1秒后恢复原样
+    });
+});
