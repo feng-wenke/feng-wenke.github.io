a92d61322f2e2cce112cb85ce50659ca452ae976
Frank Wen <126837295+feng-wenke@users.noreply.github.com>
2024-06-10 20:25:18 +0800

    Update randomplay.js

--- mysite/randomplay.js
+++ mysite/randomplay.js
@@ -59,3 +59,12 @@ function togglePlayPause() {
 }
 
 // 播放/暂停
+document.addEventListener('DOMContentLoaded', function() {
+  // 获取所有链接元素
+  var links = document.querySelectorAll('a');
+
+  // 遍历所有链接元素，并添加target="_blank"属性
+  for (var i = 0; i < links.length; i++) {
+    links[i].setAttribute('target', '_blank');
+  }
+});
